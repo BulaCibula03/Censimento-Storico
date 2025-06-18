7757f06fb0fe5e1621010ca880ccb97a05885bb6
BulaCibula03 <BulaCibula03@gmail.com>
2025-06-18 15:38:14 +0200

    Added files

---
+++ app/parsing.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def parse():
+    df = pd.read_csv('file.csv')
+    print(df)
+    return df
+
+#possiamo cominciare
+#lo stavo dicendo...
+#il backend è quella parte del sito che riguarda i processi interni, es. il passaggio da una schermata ad un'altra...
+#mentre il frontend è effettivamente quello che vedi
+#riassumendo: backend=Flask,javascript... frontend=html,css,tailwind-css
+#ci sei???
